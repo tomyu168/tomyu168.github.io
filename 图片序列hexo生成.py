e178fba971ecb1dda6ccd9da9170d22e8d1de731
tomyu168 <547301517@qq.com>
2025-01-09 16:53:25 +0800

    Site updated: 2025-01-09 16:53:14

--- 图片序列hexo生成.py
+++ 图片序列hexo生成.py
@@ -1,4 +1,4 @@
 # 将生成的文本保存到文件中
 with open("图片序列.txt", "w", encoding="utf-8") as file:
-    for x in range(22, 32):  # 从1到83
-        file.write(f"![2025-1-8-设计参考Carlyle和Deniot和Wearstler风格人工智能拓展-客厅]({x}.jpg)\n")
+    for x in range(1, 35):  # 从1到83
+        file.write(f"![2025-1-9-设计参考人工智能拓展-卧室]({x}.jpg)\n")
